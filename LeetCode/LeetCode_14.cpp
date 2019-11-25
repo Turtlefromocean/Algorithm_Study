83285a7ed4ccf2f95b660c63b6c4dcb7710d9b9e
Turtlefromocean <oliviajiyoungshin@gmail.com>
2019-11-26 01:12:34 +0900

    LeetCode_13

---
+++ LeetCode/LeetCode_14.cpp
@@ -0,0 +1,45 @@
+//
+// Created by 신지영 on 2019/11/26.
+// 14. Longest Common Prefix
+//
+
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int compare(int x, int y)   {
+    if(x <= y)  return x;
+    else return y;
+}
+string longestCommonPrefix(vector<string>& strs) {
+    if(strs.size() == 0) return "";
+
+    string prefix = strs[0];
+
+    for(int i=1;i<strs.size();i++)  {
+        int size = compare(prefix.size(), strs[i].size());
+        string answer = "";
+        for(int j=0;j<size;j++) {
+            if(prefix[j] == strs[i][j]) {
+                answer = strs[i].substr(0, j+1);
+            }
+            else {
+                break;
+            }
+
+            if(prefix.size() == 0) return "";
+        }
+        prefix = answer;
+    }
+    return prefix;
+}
+int main()  {
+
+    vector<string> arrStr;
+    // vector<string> 입력받는 부분은 생
+    string answer = longestCommonPrefix(arrStr);
+    std::cout<<answer<<endl;
+    return 0;
+}
