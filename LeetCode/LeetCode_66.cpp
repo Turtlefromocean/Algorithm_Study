d54e8a243a4b1eb71a570546f448702833cf9cfb
Turtlefromocean <oliviajiyoungshin@gmail.com>
2019-11-26 13:12:11 +0900

    LeetCode 66

---
+++ LeetCode/LeetCode_66.cpp
@@ -0,0 +1,87 @@
+//
+// Created by 신지영 on 2019/11/26.
+//
+#include <iostream>
+#include <vector>
+#include <sstream>
+
+using namespace std;
+class Solution {
+public:
+    vector<int> plusOne(vector<int>& digits) {
+
+        int n = digits.size();
+
+        for (int i=n-1;i>= 0;i--)    {
+            if (digits[i] == 9) {
+                digits[i] = 0;
+            }
+            else    {
+                digits[i]++;
+                return digits;
+            }
+        }
+
+        digits[0] =1;
+        digits.push_back(0);
+
+        return digits;
+    }
+};
+
+void trimLeftTrailingSpaces(string &input) {
+    input.erase(input.begin(), find_if(input.begin(), input.end(), [](int ch) {
+        return !isspace(ch);
+    }));
+}
+
+void trimRightTrailingSpaces(string &input) {
+    input.erase(find_if(input.rbegin(), input.rend(), [](int ch) {
+        return !isspace(ch);
+    }).base(), input.end());
+}
+
+vector<int> stringToIntegerVector(string input) {
+    vector<int> output;
+    trimLeftTrailingSpaces(input);
+    trimRightTrailingSpaces(input);
+    input = input.substr(1, input.length() - 2);
+    stringstream ss;
+    ss.str(input);
+    string item;
+    char delim = ',';
+    while (getline(ss, item, delim)) {
+        output.push_back(stoi(item));
+    }
+    return output;
+}
+
+string integerVectorToString(vector<int> list, int length = -1) {
+    if (length == -1) {
+        length = list.size();
+    }
+
+    if (length == 0) {
+        return "[]";
+    }
+
+    string result;
+    for(int index = 0; index < length; index++) {
+        int number = list[index];
+        result += to_string(number) + ", ";
+    }
+    return "[" + result.substr(0, result.length() - 2) + "]";
+}
+
+int main() {
+    string line;
+    while (getline(cin, line)) {
+        vector<int> digits = stringToIntegerVector(line);
+
+        vector<int> ret = Solution().plusOne(digits);
+
+        string out = integerVectorToString(ret);
+        cout << out << endl;
+    }
+    return 0;
+}
