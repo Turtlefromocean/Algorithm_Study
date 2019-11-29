1034e72ab90626a2c716a64b9d0213589e3d4d37
Turtlefromocean <oliviajiyoungshin@gmail.com>
2019-11-29 11:44:30 +0900

    BaekJoon 10828

---
+++ BaekJoon/BaekJoon_10828.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin>>n;
+    stack<int> stack;
+
+    for(int i=0;i<n;i++)    {
+        string s;
+        cin>>s;
+        if(s == "push") {
+            int push_number;
+            cin>>push_number;
+            stack.push(push_number);
+        }
+        else if(s == "top")   {
+            if(stack.empty())   cout<<-1<<'\n';
+            else cout<<stack.top()<<'\n';
+        }
+        else if(s == "size")    {
+            cout<<stack.size()<<'\n';
+        }
+        else if(s == "empty")   {
+            if(stack.empty()) cout<<1<<'\n';
+            else cout<<0<<'\n';
+        }
+        else if(s == "pop") {
+            if(stack.empty())   cout<<-1<<'\n';
+            else {
+                cout<<stack.top()<<'\n';
+                stack.pop();
+            }
+        }
+    }
+    return 0;
+}
